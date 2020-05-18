fdee9a81169be592c806f5afd8da14402d7d0a25
Patcas Marcel <pmarceln@yahoo.com>
2020-05-18 20:29:49 +0300

    Add auth store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule, AuthModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        AuthModule,
+        StoreModule.forRoot({}, {}),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
-import {ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from './_service/auth.service';
+import { StoreModule } from '@ngrx/store';
 
+import { LoginComponent } from './login/login.component';
+import { authReducer } from './reducers';
 
 @NgModule({
     declarations: [LoginComponent],
-    imports: [CommonModule, ReactiveFormsModule],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        StoreModule.forFeature('auth', authReducer),
+    ],
     providers: [AuthService]
 })
 export class AuthModule {}
